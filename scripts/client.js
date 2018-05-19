c6e5ebdcf14f63a156fe5deba09357391a6a1954
Flannel Bear <josh.flannelbear@gmail.com>
2018-05-19 13:27:29 -0500

    organized js

--- scripts/client.js
+++ scripts/client.js
@@ -18,7 +18,7 @@ class Employee{
     }
 } // end Employee Class
 
-// Functions
+// document ready function
 
 function setUp(){
     checkCosts();
@@ -26,34 +26,24 @@ function setUp(){
     $('#deleteButton').on('click', removeEmployee);
 } // end setUp
 
-function executeScript(){
-    // runs createEmployee with input values passed as parameters
-    createEmployee($('#firstNameInput').val(), $('#lastNameInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
-    // clears all the input fields
-    $('.input').val('');
-    // appends new data to the table
-    appendToTable();
-    // Increases monthly total payroll accordingly
-    updateMonthlyTotal();
-} // end runCreateEmployee
+// Alphabetical functions
 
-function createEmployee(firstName, lastName, idNumber, title, annualSalary){
-    // creates new employee instance
-    let employee = new Employee(firstName, lastName, idNumber, title, annualSalary);
-    // pushes new instance into employees array
-    employees.push(employee);
-    return employee;
-} // end createEmployee
-
-function updateMonthlyTotal(){
-    monthlyTotal = 0;
-    for( let i = 0; i < employees.length; i++ ){
-        monthlyTotal += Math.round(employees[i].annualSalary / 12);
+function appendToTable(){
+    $('#dataTable').empty();
+    // for loop starts at the end (could become problematic when deleting employees happens, unless we don't remove them from the array)
+    for(let i = 0; i < employees.length; i++){
+        $('#dataTable').append(`<tr id="index${i}"class=${classToggle(i)}><td class="firstname">${employees[i].firstName}</td><td class="lastname">${employees[i].lastName}</td><td class="id">${employees[i].idNumber}</td><td class="title">${employees[i].title}</td><td class="salary">${employees[i].annualSalary}</td></tr>`);
+        employees[i].position = i;
     }
-    $('#monthlyPayroll').empty();
-    $('#monthlyPayroll').append(monthlyTotal);
-    checkCosts();
-} // end updateMonthlyTotal
+} // end appendToTable
+
+function checkCosts(){
+    if(monthlyTotal < 20000){
+        $("#monthlyCosts").removeClass('red');
+    } else {
+        $('#monthlyCosts').addClass('red');
+    }
+} // end check costs
 
 function classToggle(i) {
     let toggle = 0;
@@ -74,24 +64,26 @@ function classToggle(i) {
         default:
             break;
     }
-}
+} // end class toggle
 
-function appendToTable(){
-    $('#dataTable').empty();
-    // for loop starts at the end (could become problematic when deleting employees happens, unless we don't remove them from the array)
-    for(let i = 0; i < employees.length; i++){
-        $('#dataTable').append(`<tr id="index${i}"class=${classToggle(i)}><td class="firstname">${employees[i].firstName}</td><td class="lastname">${employees[i].lastName}</td><td class="id">${employees[i].idNumber}</td><td class="title">${employees[i].title}</td><td class="salary">${employees[i].annualSalary}</td></tr>`);
-        employees[i].position = i;
-    }
-} // end appendToTable
+function createEmployee(firstName, lastName, idNumber, title, annualSalary){
+    // creates new employee instance
+    let employee = new Employee(firstName, lastName, idNumber, title, annualSalary);
+    // pushes new instance into employees array
+    employees.push(employee);
+    return employee;
+} // end createEmployee
 
-function checkCosts(){
-    if(monthlyTotal < 20000){
-        $("#monthlyCosts").removeClass('red');
-    } else {
-        $('#monthlyCosts').addClass('red');
-    }
-} // end check costs
+function executeScript(){
+    // runs createEmployee with input values passed as parameters
+    createEmployee($('#firstNameInput').val(), $('#lastNameInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
+    // clears all the input fields
+    $('.input').val('');
+    // appends new data to the table
+    appendToTable();
+    // Increases monthly total payroll accordingly
+    updateMonthlyTotal();
+} // end runCreateEmployee
 
 function removeEmployee() {
     let employeeID = $('#deleteInput').val();
@@ -106,10 +98,14 @@ function removeEmployee() {
             alert('Input valid Employee ID');
         }
     }
-}
+} // end removeEmployee
 
-
-
-
-
-
+function updateMonthlyTotal(){
+    monthlyTotal = 0;
+    for( let i = 0; i < employees.length; i++ ){
+        monthlyTotal += Math.round(employees[i].annualSalary / 12);
+    }
+    $('#monthlyPayroll').empty();
+    $('#monthlyPayroll').append(monthlyTotal);
+    checkCosts();
+} // end updateMonthlyTotal
