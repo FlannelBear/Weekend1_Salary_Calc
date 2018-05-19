7437d30a741e379654b9ff1751a050f8295dbb19
Flannel Bear <josh.flannelbear@gmail.com>
2018-05-18 20:12:59 -0500

    input fields clear after submitting

--- scripts/client.js
+++ scripts/client.js
@@ -23,8 +23,11 @@ function setUp(){
 } // end setUp
 
 function runCreateEmployee(){
+    // runs createEmployee with input values passed as parameters
     createEmployee($('#firstNameInput').val(), $('#lastNameInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
-}
+    // clears all the input fields
+    $('.input').val('');
+} // end runCreateEmployee
 
 function createEmployee(firstName, lastName, idNumber, title, annualSalary){
     let employee = new Employee(firstName, lastName, idNumber, title, annualSalary);
