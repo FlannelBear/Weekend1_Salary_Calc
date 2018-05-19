69f7af14d8f787ec4c64b040f6c6d20c5aba63e7
Flannel Bear <josh.flannelbear@gmail.com>
2018-05-19 09:53:50 -0500

    edited code for redundant blocks

--- scripts/client.js
+++ scripts/client.js
@@ -20,37 +20,40 @@ class Employee{
 // Functions
 
 function setUp(){
-    $('#submitBtn').on('click', executeSubmit);
+    $('#submitBtn').on('click', executeScript);
 } // end setUp
 
-function executeSubmit(){
+function executeScript(){
     // runs createEmployee with input values passed as parameters
     createEmployee($('#firstNameInput').val(), $('#lastNameInput').val(), $('#idInput').val(), $('#titleInput').val(), $('#annualSalaryInput').val());
     // clears all the input fields
     $('.input').val('');
+    // appends new data to the table
     appendToTable();
+    // Increases monthly total payroll accordingly
     updateMonthlyTotal();
 } // end runCreateEmployee
 
 function createEmployee(firstName, lastName, idNumber, title, annualSalary){
+    // creates new employee instance
     let employee = new Employee(firstName, lastName, idNumber, title, annualSalary);
+    // pushes new instance into employees array
     employees.push(employee);
     return employee;
 } // end createEmployee
 
 function appendToTable(){
+    // for loop starts at the end (could become problematic when deleting employees happens, unless we don't remove them from the array)
     for(let i = employees.length-1; i < employees.length; i++){
-        let employee = employees[i];
-        $('table').append(`<tr id="data"><td>${employee.firstName}</td><td>${employee.lastName}</td><td>${employee.idNumber}</td><td>${employee.title}</td><td>${employee.annualSalary}</td></tr>`);
+        $('table').append(`<tr id="data"><td>${employees[i].firstName}</td><td>${employees[i].lastName}</td><td>${employees[i].idNumber}</td><td>${employees[i].title}</td><td>${employees[i].annualSalary}</td></tr>`);
+        updateMonthlyTotal(employees[i]);
     }
 } // end appendToTable
 
-function updateMonthlyTotal(){
-    for( let i = employees.length-1; i < employees.length; i++ ){
-        monthlyTotal += Math.round((employees[i].annualSalary / 12));
+function updateMonthlyTotal(employee){
+        monthlyTotal += Math.round((employee.annualSalary / 12));
         $('#monthlyPayroll').empty();
         $('#monthlyPayroll').append(monthlyTotal);
-    }
 } // end updateMonthlyTotal
 
 
